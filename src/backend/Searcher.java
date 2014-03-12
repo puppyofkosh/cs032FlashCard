9151193be80ddd5eb2ad89ac35dc66ebbb95d468
puppyofkosh <puppyofkosh@kosh-ThinkPad-Z61t.(none)>
2014-03-12 00:38:07 -0400

    Removed a bunch of class files

--- src/backend/Searcher.java
+++ src/backend/Searcher.java
@@ -4,9 +4,20 @@ import java.util.List;
 
 import flashcard.FlashCard;
 
+
+/***
+ * Allow us to perform search on the Resources
+ * @author puppyofkosh
+ *
+ */
 public interface Searcher {
 	
 	// Needs to be initialized with a Resources obj
 	
+	/**
+	 * Grab all flashcards considered to be relevant to the query
+	 * @param query
+	 * @return
+	 */
 	List<FlashCard> search(String query);
 }
