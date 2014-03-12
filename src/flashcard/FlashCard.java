a9dadc0134c2fe29b142de0d8976c612c35f2821
Samuel Kortchmar <samuel_kortchmar@brown.edu>
2014-03-12 15:42:22 -0400

    Added comments to FlashCard stuff

--- src/flashcard/FlashCard.java
+++ src/flashcard/FlashCard.java
@@ -1,24 +1,76 @@
 package flashcard;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
+/**
+ * 
+ * @author samkortchmar/ian boros/peter krishner
+ *The interface for the FlashCard objects that are the core of this project
+ *Probably we will only have one class that implements this interface, but
+ *we wanted to make it an interface to create stubs.
 
+ */
 public interface FlashCard {
+
+/*FlashCards will be created only after all the information in the constructors
+ * below has been collected by our FlashCardFactory class - this way we avoid
+ * problems with cards that are in intermediate states. When a FlashCard is created
+ * in this way it creates a FlashCard on the filesystem.*/
+		/*FlashCard(String name, AudioFile question, AudioFile answer,
+		 *  int interval, List<String> tags, String set);*/	
+		/*FlashCard(String name, AudioFile question, String questionText,
+		 *  AudioFile answer, String answerText, int interval, List<String> tags,  String set);*/
 	
-/*FlashCard(String name, AudioFile question, AudioFile answer,
- *  int interval, List<String> tags, String set);*/	
-/*FlashCard(String name, AudioFile question, String questionText,
- *  AudioFile answer, String answerText, int interval, List<String> tags,  String set);*/
+/*This constructor builds a FlashCard object form the filePath to the FlashCard on the filesystem.
+		/*FlashCard(String filePath)*/
+
 	
-	// Getters for sets, tags, interval
 	
-	List<FlashCardSet> getSets();
-	void addSet(FlashCardSet s) throws IOException;
+	/**
+	 * This method gets all sets a given FlashCard is a member of. Note this
+	 * information is not actually stored in the FlashCard object, but in
+	 * the sets.info file we will maintain.
+	 * @return - A collection of all the Sets this FlashCard is a member of.
+	 */
+	Collection<FlashCardSet> getSets();
 	
-	List<String> getTags();
+	/**
+	 * This method gets all the tags that this FlashCard has attached to it.
+	 * This information will be retrieved from the FlashCard Metadata file
+	 * in the FlashCard folder.
+	 * @return - A collection of the tags associated with this FlashCard.
+	 */
+	Collection<String> getTags();
+	
+	/**
+	 * This method adds a tag to the current FlashCard by adding the tag string
+	 * to the current flashCards Metadata file. It also adds the card-tag pair 
+	 * to the current tag index file.
+	 * @param tag - the tag to be added.
+	 * @throws IOException - if the current FlashCard's Metadata file cannot be
+	 * found.
+	 */
 	void addTag(String tag) throws IOException;
 	
+	/**
+	 * Returns the interval for the current FlashCard. This information will be
+	 * in the metaData file but also be loaded into the buffered FlashCards from
+	 * getFlashCards in Resources.
+	 * @return - the interval between the question and the answer of the card.
+	 */
 	int getInterval();
+	
+	/**
+	 * Sets the interval of the current FlashCard. This method is for 
+	 * retroactively setting the card's interval, after it has been made and
+	 * read by the Resources class. This method will affect both the buffered
+	 * version and on-disk version of the FlashCard, hence the IOException
+	 * 
+	 * @param interval - the interval to be set.
+	 * @throws IOException - if the filesystem does not contain the specified
+	 * FlashCard.
+	 */
 	void setInterval(int interval) throws IOException;
 
 }
--- src/flashcard/FlashCardSet.java
+++ src/flashcard/FlashCardSet.java
@@ -1,15 +1,41 @@
 package flashcard;
-
-import java.util.List;
+import java.io.IOException;
+import java.util.Collection;
 
 public interface FlashCardSet {
-	// getters for global tags
 	
-	//?
-	List<FlashCard> getAll();
+	/**
+	 * We can use this method to get all flash cards in the FlashCardSet.
+	 * This method will be called by a wide variety of other classes - 
+	 * for example, it will be used by the GUI to represent flashcards, as
+	 * @return - a Collection of FlashCards.
+	 */
+	Collection<FlashCard> getAll();
 	
-	void addTag(String tag);
-	void removeTag(String tag);
 	
-	List<String> getTags();
+	/**
+	 * Adds a "global" tag to the FlashCardSet - i.e. a tag that applies for
+	 * all cards in the set. This method affects the buffered FlashCardSet
+	 * as well as its on-disk representation. It also adds the tag and all
+	 * cards in the set to the tagIndex file.
+	 * @param tag - the global tag to be added.
+	 * @throws IOException - if there is trouble with the requisite on-disk files 
+	 */
+	void addTag(String tag) throws IOException;
+	
+	/**
+	 * Removes a "global" tag from the FlashCardSet. This method affects the
+	 * buffered FlashCardSet as well as its on-disk representation. It also 
+	 * removes the tag and all cards in the set from the tagIndex file.
+	 * @param tag - the global tag to be removed.
+	 * @throws IOException - if there is trouble with the requisite on-disk files 
+	 */
+	void removeTag(String tag) throws IOException;
+	
+	/**
+	 * Displays all the tags for the specific FlashCard set. Draws information
+	 * exclusively from the buffered FlashCardSet
+	 * @return - a list of the strings in question.
+	 */
+	Collection<String> getTags();
 }
