f9367e85b236f477a649d2eff52ca63449939d72
pkirschn <peter_kirschner@brown.edu>
2014-03-12 00:30:01 -0400

    Commented audio interfaces and created util package

--- src/audio/AudioFile.java
+++ src/audio/AudioFile.java
@@ -2,6 +2,14 @@ package audio;
 
 import javax.sound.sampled.AudioInputStream;
 
+/**
+ * A representation of an AudioFile that can be used within the program
+ */
 public interface AudioFile {
+	/**
+	 * Gets the AudioInputStream for this AudioFile.
+	 * This might be used for combining AudioFiles, or for playing an AudioFile.
+	 * @return The AudioInputStream for this AudioFile
+	 */
 	AudioInputStream getStream();
 }
--- src/util/Constants.java
+++ src/util/Constants.java
@@ -1,7 +1,10 @@
-package audio;
+package util;
 
 import javax.sound.sampled.AudioFormat;
 
+/**
+ * Contains the constants used in the project
+ */
 public class Constants {
 
 //	AudioFormat standardizedFormat = .... 
