231226244a3c44b0efc5376225cb97d4612280db
puppyofkosh <puppyofkosh@kosh-ThinkPad-Z61t.(none)>
2014-03-12 21:50:11 -0400

    Added really brief test for text to speech reader, and started diagrams

---
+++ src/audio/TextToSpeechReaderTest.java
@@ -0,0 +1,22 @@
+package audio;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TextToSpeechReaderTest {
+
+	@Test
+	public void testTTS() {
+//		
+//		TextToSpeechReader reader = new FreeTTSReader();
+//	
+//		AudioFile helloWorld = reader.read("hello world");
+//		AudioFile actualHelloWorld = new AudioFileFromFile(new File("tests/helloWorld.wav"));
+//		
+//		assertTrue(helloWorld.equals(actualHelloWorld));
+//		
+//		// Obviously we'd write a number of test cases nearly identical to this one
+	}
+
+}
