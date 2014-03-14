2bcee4d5255f1ae8787a08e394fd452f73359806
puppyofkosh <puppyofkosh@kosh-ThinkPad-Z61t.(none)>
2014-03-14 14:56:03 -0400

    Added gui stuff

---
+++ src/gui/ExportPanel.java
@@ -0,0 +1,23 @@
+package gui;
+
+import javax.swing.JPanel;
+
+/**
+ * Display the interfaces for exporting cards. Should have the options (possibly using other panels)
+ * to export to the database, export to "flat" mp3 files, etc.
+ * 
+ * See gui layout for an idea of what specific swing components this would need
+ * 
+ * ian
+ * 
+ * @author puppyofkosh
+ *
+ */
+public class ExportPanel extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/gui/ImportPanel.java
@@ -0,0 +1,23 @@
+package gui;
+
+import javax.swing.JPanel;
+
+/**
+ * Display the interfaces for importing cards. Should have the options (possibly using other panels)
+ * to import from file, database or from quizlet
+ * 
+ * See gui layout for an idea of what specific swing components this would need
+ * 
+ * ian
+ * 
+ * @author puppyofkosh
+ *
+ */
+public class ImportPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
