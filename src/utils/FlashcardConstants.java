b9fec2e4636de3db6cf963c72408d0cce6ab4cd5
skortchmark9 <samuel_kortchmar@brown.edu>
2014-04-16 20:33:41 -0400

    Useful stuff consolidated into a single util package

---
+++ src/utils/FlashcardConstants.java
@@ -0,0 +1,12 @@
+package utils;
+
+import javax.sound.sampled.AudioFormat;
+
+/**
+ * Contains the constants used in the project
+ */
+public class FlashcardConstants {
+
+public static final	AudioFormat standardizedFormat = new AudioFormat(8000.0F, 8, 1, true, true);
+	
+}
