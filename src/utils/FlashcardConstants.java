448db993b5da2638e2b26bd8d8e3f5868057f0ad
pkirschn <peter_kirschner@brown.edu>
2014-04-17 09:21:27 -0400

    fix standardized format to match recording with tts

--- src/utils/FlashcardConstants.java
+++ src/utils/FlashcardConstants.java
@@ -2,12 +2,14 @@ package utils;
 
 import javax.sound.sampled.AudioFormat;
 
+import audio.AudioConstants;
+
 /**
  * Contains the constants used in the project
  */
 public class FlashcardConstants {
 
-	public static final	AudioFormat standardizedFormat = new AudioFormat(8000.0F, 8, 1, true, true);
+	public static final	AudioFormat standardizedFormat = AudioConstants.TTSREADER;
 
 	public static final String METADATA_HEADER = "NAME\tINTERVAL\tSETS\tTAGS";
 	public final static String CARDS_FOLDER = "./Example FileSystem/Application Data/CARDS";
