33261cd8bc48d19b6508c41dd03c7cbc9c962169
puppyofkosh <puppyofkosh@kosh-ThinkPad-Z61t.(none)>
2014-04-16 23:55:16 -0400

    Set up Sam's magical flashcard implementation that uses the human readable format. Example in SimpleFactory's main method

---
+++ src/flashcard/LocallyStoredFlashCard.java
@@ -0,0 +1,84 @@
+package flashcard;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import audio.AudioFile;
+
+
+/**
+ * Somewhat legit implementation of a flashcard that stores its data in memory
+ * and on file, only writing/reading to file when updates are made
+ * @author puppyofkosh
+ *
+ */
+public class LocallyStoredFlashCard implements FlashCard{
+
+	public static class Data
+	{
+		public String name = "", sets = "", pathToFile = "";
+		public String questionText = "", answerText = "";
+		public List<String> tags = Arrays.asList();
+		public AudioFile question, answer;
+		public int interval = 0;
+	}
+	
+	private Data data;
+	
+	public LocallyStoredFlashCard(Data data)
+	{
+		this.data = data;
+	}
+	
+	
+	@Override
+	public String getName() {
+		return data.name;
+	}
+
+	// FIXME: Implement
+	@Override
+	public Collection<FlashCardSet> getSets() {
+		return null;
+	}
+
+	@Override
+	public Collection<String> getTags() {
+		return data.tags;
+	}
+
+	@Override
+	public void addTag(String tag) throws IOException {
+		data.tags.add(tag);
+		SimpleFactory.writeCard(this);
+	}
+
+	@Override
+	public int getInterval() {
+		return data.interval;
+	}
+
+	@Override
+	public void setInterval(int interval) throws IOException {
+		data.interval = interval;
+		SimpleFactory.writeCard(this);
+	}
+
+	@Override
+	public String getPath() {
+		return data.pathToFile;
+	}
+
+	@Override
+	public AudioFile getQuestionAudio() {
+		return data.question;
+	}
+
+	@Override
+	public AudioFile getAnswerAudio() {
+		return data.answer;
+	}
+
+}
