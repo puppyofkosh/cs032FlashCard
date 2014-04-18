334e1320a42a31a34cce4ddb22df9ff6b1ed5ba4
pkirschn <peter_kirschner@brown.edu>
2014-04-18 13:14:20 -0400

    fixed small error, made ExportPanel more user friendly

--- src/gui/ExportPanel.java
+++ src/gui/ExportPanel.java
@@ -7,6 +7,7 @@ import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
@@ -89,26 +90,46 @@ public class ExportPanel extends JPanel {
 			public void actionPerformed(ActionEvent arg0) {
 				_cardTable.updateSelectedCards();
 				List<FlashCard> cards = _cardTable.getSelectedCards();
+				
+				if (cards.isEmpty()) {
+					JOptionPane.showMessageDialog(panel, "You must pick which cards to export!");
+					return;
+				}
+				
 				if (rdbtnWav.isSelected()) {
 					try {
 						wavExporter.export(cards);
+						JOptionPane.showMessageDialog(panel, "Audio has been created");
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 				}
 				
-				if (rdbtnItunes.isSelected()) {
+				else if (rdbtnItunes.isSelected()) {
 					try {
-						itunesExporter = new ItunesExporter(new File(groupName.getText() + ".m3u"));
+						String playlist = JOptionPane.showInputDialog(panel, "Choose name of playlist");
+						if (playlist == null || playlist == "") {
+							JOptionPane.showMessageDialog(panel, "Playlist not created because no name was selected");
+							return;
+						}
+						itunesExporter = new ItunesExporter(new File(playlist + ".m3u"));
 					
 					itunesExporter.export(cards);
+					JOptionPane.showMessageDialog(panel, "Playlist has been created! To use this playlist, open Itunes and click Import Playlist, and choose the file \'"
+					+ playlist + ".m3u\' in the directory this program is located in.");
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 				}
 				
+				else if(rdbtnNetwork.isSelected()) {
+					JOptionPane.showMessageDialog(panel, "Not yet implemented. Sorry");
+				}
+				
+				else
+					JOptionPane.showMessageDialog(panel, "You must choose what kind of export to perform!");
 			}
 			
 		});
@@ -116,13 +137,6 @@ public class ExportPanel extends JPanel {
 		
 		panel = new JPanel();
 		add(panel);
-		
-		lblGroupName = new JLabel("Group Name: ");
-		panel.add(lblGroupName);
-		
-		groupName = new JTextField();
-		panel.add(groupName);
-		groupName.setColumns(10);
 		
 		btnChangeDestination = new JButton("Change Audio Destination");
 		panel.add(btnChangeDestination);
