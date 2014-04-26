1c113fd6722792c01aa50b683f1e4f8c5e35eb34
skortchmark9 <samuel_kortchmar@brown.edu>
2014-04-26 13:10:36 -0400

    Lots of changes made to recordPanel, most others ancillary

---
+++ src/gui/GuiConstants.java
@@ -0,0 +1,18 @@
+package gui;
+
+import java.awt.Color;
+
+public class GuiConstants {
+
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 500;
+
+	public static final String IMPORT_PANEL_NAME = "import panel";
+	public static final String EXPORT_PANEL_NAME = "export panel";
+	public static final String CREATE_PANEL_NAME = "create panel";
+	public static final String FLASHBOARD_PANEL_NAME = "flashboard panel";
+	
+	public static final Color BLACK = Color.BLACK;
+	public static final Color CARD_BACKGROUND = Color.CYAN;
+
+}
