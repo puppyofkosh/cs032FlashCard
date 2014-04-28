6b79ee66189bc245efbc7d01e340324fa1053434
skortchmark9 <samuel_kortchmar@brown.edu>
2014-04-28 16:28:40 -0400

    Added default Icon sizes.

--- src/gui/GuiConstants.java
+++ src/gui/GuiConstants.java
@@ -10,6 +10,7 @@ public class GuiConstants {
 	public static final String IMPORT_PANEL_NAME = "import panel";
 	public static final String EXPORT_PANEL_NAME = "export panel";
 	public static final String CREATE_PANEL_NAME = "create panel";
+	public static final String SET_CREATION_PANEL_NAME = "set creation panel";
 	public static final String FLASHBOARD_PANEL_NAME = "flashboard panel";
 	
 	public static final Color BLACK = Color.BLACK;
@@ -18,4 +19,7 @@ public class GuiConstants {
 	//Some default settings for tag label.
 	public static final int DEFAULT_TAG_LABEL_ROUNDEDNESS = 10;
 	public static final int DEFAULT_BUTTON_SIZE = 15;
+	public static final int MAX_TAG_LENGTH = 30;
+	
+	public static final int SOURCELIST_ICON_SIZE = 16;
 }
