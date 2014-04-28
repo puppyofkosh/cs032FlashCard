459cde350869e6fcb38a4d64067d8e34be6cbb7e
skortchmark9 <samuel_kortchmar@brown.edu>
2014-04-28 16:33:32 -0400

    Now demos sourceList

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,21 +1,18 @@
 package gui;
 
 import java.awt.EventQueue;
-import java.util.Arrays;
 
 import javax.swing.JFrame;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					JFrame frame = new JFrame();
 					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-					final TagPanel box = new TagPanel(Arrays.asList(new String[] {"Hat", "Cat", "Dog", "Wanderlust", "Sleep"}));
-					
-					frame.add(box);
+					frame.add(new SetBrowser());
 					frame.pack();
 					frame.setVisible(true);
 				} catch (Exception e) {
