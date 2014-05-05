7c1317990b2a4583f157e2259c3a05ef12df2367
skortchmark9 <samuel_kortchmar@brown.edu>
2014-05-05 00:15:15 -0400

    removed buttons added right click.

--- src/gui/FlashCardPanel.java
+++ src/gui/FlashCardPanel.java
@@ -1,32 +1,21 @@
 package gui;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.io.IOException;
-import java.text.ParseException;
-import java.util.Collection;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JSpinner;
-import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingConstants;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 
-import utils.Writer;
 import controller.Controller;
 import flashcard.FlashCard;
-import flashcard.FlashCardSet;
-import gui.IconFactory.IconType;
 
 /**
  * This class is used by the GUI to represent flashCards, as
@@ -37,26 +26,18 @@ import gui.IconFactory.IconType;
 public class FlashCardPanel extends JPanel {
 	private static final long serialVersionUID = 2666411116428918076L;
 	private FlashCard _card;
-
 	private JPanel _headerPanel;
 	JLabel _cardName;
-	private JLabel _delete;
-
-	private JPanel _interactionPanel;
 	ImageToggleButton _playAndStop;
-	private JSpinner _spinner;
 	private TagPanel _tagPanel;
 
 	/**
 	 * This method is called so we can reuse FlashCardPanel.
 	 * @param card
 	 */
-	public void reinitialize(FlashCard card)
-	{
+	public void reinitialize(FlashCard card) {
 		_card = card;
 		_cardName.setText(card.getName());
-		_spinner.setValue(card.getInterval());
-		populateSets(card.getSets());
 		_tagPanel.reinitialize(card);
 		revalidate();
 	}
@@ -68,15 +49,11 @@ public class FlashCardPanel extends JPanel {
 	public FlashCardPanel(FlashCard card) {
 
 		String defaultName = card.getName();
-		int defaultInterval = card.getInterval();
-		Collection<FlashCardSet> defaultSets = card.getSets();
-
 		setPreferredSize(new Dimension(225, 150));
 		_card = card;
 		setBackground(GuiConstants.CARD_BACKGROUND);
 		setBorder(BorderFactory.createMatteBorder(0, 0, 2, 2, Color.WHITE));
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
 
 		//The header will contain the given card's name and a delete button.
 		_headerPanel = new JPanel();
@@ -86,79 +63,15 @@ public class FlashCardPanel extends JPanel {
 
 		_cardName = new JLabel(defaultName, SwingConstants.CENTER);
 		_headerPanel.add(_cardName);
-		_headerPanel.add(new SetSelectionButton("Modify Sets", _card));
-
-		//Initialize the delete card button.
-		_delete = new JLabel(IconFactory.loadIcon(IconType.DELETE, GuiConstants.DEFAULT_BUTTON_SIZE, false));
-		_delete.setHorizontalAlignment(SwingConstants.TRAILING);
-		_delete.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
-
-		//This mouse listener adds functionality to the X that appears next to.
-		//the Card's name.
-		_delete.addMouseListener(new MouseListener() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				_delete.setBorder(BorderFactory.createLineBorder(Color.RED, 1));
-			}
-
-			@Override
-			public void mousePressed(MouseEvent e) {
-				_delete.setBorder(BorderFactory.createLineBorder(Color.RED, 1));
-			}
-
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				Controller.deleteCard(_card);
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				_delete.setBorder(BorderFactory.createLineBorder(Color.RED, 1));
-			}
-			@Override
-			public void mouseExited(MouseEvent e) {		
-				_delete.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
-			}
-		});
-
-		_headerPanel.add(_delete, BorderLayout.EAST);
 		add(_headerPanel);
 
 		//This panel displays the interval as well as a play button which plays
 		//the card as you might hear it after you export it, including the
 		//assigned interval between the question and answer.
-		_interactionPanel = new JPanel();
-		_interactionPanel.setOpaque(false);
 
-		JLabel lblInterval = new JLabel("Interval");
-		_interactionPanel.add(lblInterval);
-		_spinner = new JSpinner(new SpinnerNumberModel(defaultInterval, 0, 10, 1));
-		_interactionPanel.add(_spinner);
 		_playAndStop = ImageToggleButton.playStopButton();
-		_interactionPanel.add(_playAndStop);
 		_playAndStop.addActionListener(new PlayStopListener(_playAndStop));
-
-		JSpinner.DefaultEditor editor = ((JSpinner.DefaultEditor) _spinner.getEditor());
-		editor.getTextField().setColumns(2);
-		editor.getTextField().setEditable(false);
-
-
-		_spinner.addChangeListener(new ChangeListener() {
-			@Override
-			public void stateChanged(ChangeEvent e) {
-				try {
-					_spinner.commitEdit();
-					int newValue = (int) _spinner.getValue();
-					_card.setInterval(newValue);
-				} catch (ParseException | IOException e1) {
-					Writer.err("ERROR: Could not write change to card - reverting to original interval");
-					_spinner.setValue(_card.getInterval());
-				}
-			}
-		});
-
-		_interactionPanel.setMaximumSize(_interactionPanel.getPreferredSize());
-		add(_interactionPanel);
+		_headerPanel.add(_playAndStop);
 
 		//This panel maintains all information about the tags and displays them
 		//as bubbles in a scroll window. When you mouse over it, it will display
@@ -168,21 +81,33 @@ public class FlashCardPanel extends JPanel {
 		//A ControlScrollPane allows us to responsively scroll through cards as
 		//well as tags.
 		JScrollPane js = new ControlScrollPane(_tagPanel);
+		js.getVerticalScrollBar().setPreferredSize(new Dimension(0,0));
 		js.setOpaque(false);
 		js.getViewport().setOpaque(false);
 		js.setBorder(BorderFactory.createEmptyBorder());
 		add(js);
-
-		populateSets(defaultSets);
+		addMouseListener(createRightClickMenu());
+		_tagPanel.addMouseListener(createRightClickMenu());
 		revalidate();
 	}
 
-
-	/**
-	 * As yet unimplemented
-	 * @param sets
-	 */
-	private void populateSets(Collection<FlashCardSet> sets) {}
+	private PopupListener createRightClickMenu() {
+		JMenuItem edit = new JMenuItem("Edit Card");
+		edit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Controller.editCard(_card);
+			}
+		});
+		JMenuItem delete = new JMenuItem("Delete Card");
+		delete.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Controller.deleteCard(_card);
+			}
+		});
+		return new PopupListener(edit, delete);
+	}
 
 
 	/**
